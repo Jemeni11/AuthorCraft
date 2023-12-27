882aef5931e41b77bf33bfbc714b005206fc292a
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-12-27 13:23:24 +0100

    Update tailwind configuration and add utility function

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -7,6 +8,7 @@ module.exports = {
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
     container: {
       center: true,
@@ -18,16 +20,16 @@ module.exports = {
     extend: {
       colors: {
         "hanko-red": "#FF2E4C",
-        "hanko-blue": "#040c25"
+        "hanko-blue": "#040c25",
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
         },
       },
       animation: {
@@ -37,4 +39,6 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/forms")],
-};
+} satisfies Config;
+
+export default config;
